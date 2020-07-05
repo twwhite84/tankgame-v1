c49114b0236ac6889e2c2c110281bbffb46deefa
Tom White <twwhite.pvt@gmail.com>
2020-07-05 17:37:07 +1200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('main.js loaded');
+
+document.getElementById('topbar-fire-btn').addEventListener('click', function()
+{
+	console.log('fire button clicked');
+});
+
+// document.getElementsByTagName('body')[0].height = window.innerHeight;
+// let innerWindow = window.innerHeight;
+// let myFontSize = parseInt(window.getComputedStyle(document.body).getPropertyValue('font-size'));
+// document.getElementById('playfield').height = window.innerHeight;
