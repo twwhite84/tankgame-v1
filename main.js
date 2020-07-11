5687a43a7c9c24bf0ec7fdc534312402086fe3f8
Tom <twwhite.pvt@gmail.com>
2020-07-11 15:48:34 +1200

    wip_1548_11-07

--- main.js
+++ main.js
@@ -1,13 +1,48 @@
-import { Game } from './modules/game.js';
-import { UI } from './modules/UI.js';
+//main.js
 
-//set up event listeners
-let DOMElements =
+import { UI } from './modules/UI.js';
+import { Game } from './modules/game.js';
+
+let dom =
 {
-	canvas:		document.getElementById(`playfield`),
-	ctx:			document.getElementById(`playfield`).getContext(`2d`),
-	fireBtn:	document.querySelectorAll(`.fire-btn`)
-							.forEach(el => el.addEventListener(`click`, this.fireButton)),
-	startBtn:	document.getElementById(`mainmenu-start-btn`)
-							.addEventListener(`click`, this.startButton)
+	canvas:			document.getElementById(`playfield`),
+	ctx:				document.getElementById(`playfield`).getContext(`2d`),
+	buttons:
+	{
+		fire:
+		{
+			fireTB: document.getElementById(`topbar-fire-btn`),
+			fireSB:	document.getElementById(`sidebar-fire-btn`),
+		},
+		
+		start:		document.getElementById(`mainmenu-start-btn`),
+	},
+	
+	inputs:
+	{
+		p1name:		document.getElementById(`mainmenu-input-pone`),
+		p2name: 	document.getElementById(`mainmenu-input-ptwo`)
+	}
 }
+
+//set up listeners
+let ui = new UI(dom);
+
+let fireButtons = dom.buttons.fire;
+Object.keys(fireButtons).forEach(function(item) { console.log(fireButtons[item] }));
+// dom.buttons.fire.forEach(el=>el.addEventListener(`click`, fire));
+// dom.buttons.startBtn.addEventListener(`click`, start);
+
+
+function start()
+{
+	console.log(`a start button was clicked`);
+	// ui.updatePlayers(dom.inputs.pOneName, dom.inputs.pTwoName);
+}
+
+
+function fire()
+{
+	console.log(`a fire button was clicked`);
+}
+
--- modules/UI.js
+++ modules/UI.js
@@ -1,45 +1,35 @@
-import { Game } from './game.js';
-
-//this module contains methods for the view
-
+//this module has to do with GUI updates
 class UI
 {
-	#DOMElements;
-	#playfieldSize;
+	#dom;
+	#p1;
+	#p2;
 	
-	constructor()
+	constructor(dom)
 	{
-		this.#DOMElements =
-		{
-			canvas:		document.getElementById(`playfield`),
-			ctx:			document.getElementById(`playfield`).getContext(`2d`),
-			fireBtn:	document.querySelectorAll(`.fire-btn`).forEach(el => el.addEventListener(`click`, this.fireButton)),
-			startBtn:	document.getElementById(`mainmenu-start-btn`).addEventListener(`click`, this.startButton)
-		}
-		
-		//value of 400 seems to work best across various screens, not too pixelly/blurry
-		this.#DOMElements.canvas.width = this.#playfieldSize;
-		this.#DOMElements.canvas.height = this.#playfieldSize;
+			this.#dom = dom;
 	}
 	
-	getDOMElements()
+	
+	updatePlayers(p1, p2)
 	{
-		return this.#DOMElements;
+		this.#p1 = p1;
+		this.#p2 = p2;
 	}
 	
-	fireButton()
+	
+	btnFire()
 	{
-		console.log(`fire button should call a firing method on the game class`);
+		console.log(`fire button clicked`);
 	}
 	
-	startButton()
+	
+	btnStart()
 	{
 		document.getElementById('mainmenu').style.display = 'none';
-		console.log(`start button should call something on the game class`);
 		let p1 = document.getElementById('mainmenu-player-one').value;
 		let p2 = document.getElementById('mainmenu-player-two').value;
 		let currentGame = new Game(p1, p2);
-		currentGame.testLoop(this.#playfieldSize);
 	}
 	
 	plotPixel(x, y)
@@ -50,7 +40,7 @@ class UI
 		{
 			myImageData.data[i] = 255;
 		}
-		this.#DOMElements[ctx].putImageData(myImageData, Math.round(x), Math.round(y));
+		// this.#DOMElements[ctx].putImageData(myImageData, Math.round(x), Math.round(y));
 	}
 }
 
