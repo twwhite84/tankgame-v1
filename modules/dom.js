53c20c4e6e356bc7b98e0932f75d569330d81124
Tom <twwhite.pvt@gmail.com>
2020-07-12 18:16:59 +1200

    wip_1816_120720

---
+++ modules/dom.js
@@ -0,0 +1,30 @@
+export const Dom =
+{
+	playfield:			document.getElementById(`playfield`),
+	playfieldCtx:		document.getElementById(`playfield`).getContext(`2d`),
+	buttons:
+	{
+		fire:
+		{
+			fireTB: 		document.getElementById(`topbar-fire-btn`),
+			fireSB:			document.getElementById(`sidebar-fire-btn`)
+		},
+		
+		start:				document.getElementById(`mainmenu-start-btn`),
+	},
+	
+	inputs:
+	{
+		p1name:				document.getElementById(`mainmenu-pone-input`),
+		p2name: 			document.getElementById(`mainmenu-ptwo-input`)
+	},
+	
+	labels:
+	{
+		player:
+		{
+			currentTB:	document.getElementById(`topbar-player-current`),
+			currentSB:	document.getElementById(`sidebar-player-current`)
+		}
+	}
+}
